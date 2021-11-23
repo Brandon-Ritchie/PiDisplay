8d6f6b2849219115e62b465487d0898a35209512
Brandon-Ritchie <britchie@brandonritchie.tech>
2021-11-22 21:57:08 -0500

    Added components for home page and removed some default react files

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../Home/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
