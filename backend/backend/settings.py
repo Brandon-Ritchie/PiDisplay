cabe516fd29cf0ca64fd3bc93fa85f45fb36d7ac
Brandon Ritchie <brandonritchie@GreenRoomAP.localdomain>
2021-12-14 16:34:16 -0500

    Remove Django debug and browseable API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -155,7 +155,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
