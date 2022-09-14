4f55c9395224a4d3da9056f91835bca519a40531
Brandon-Ritchie <ritchietrumpet@gmail.com>
2022-09-14 19:41:12 -0400

    Initial commit

---
+++ apps/pi-display-manager/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pi-display-web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pi-display-manager';
+}
